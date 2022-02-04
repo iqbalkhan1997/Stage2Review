3a0928644d13823c90c501937f67f6f96f52c167
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-02-04 09:58:26 +0530

    Stage_2_review

---
+++ headsandtails.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -x
+flip=1;
+heads=0;
+tails=0;
+
+while [[ $heads -ne 22 && $tails -ne 22 ]]
+do
+coin=$((RANDOM%2));
+	if [ $coin -eq 0 ]
+	then
+#	echo "heads"$heads
+	((heads++))
+	else
+	((tails++))
+#	echo "tails   $tails"
+fi
+	if [ $heads -eq 21 ]
+	then
+		echo "heads won by $(($heads-$tails)) times"
+		echo "head flipped 21 times"
+		break;
+	elif [ $tails -eq 21 ]
+	then
+		echo " tails won by $(($tails-$heads)) times "
+		echo "tail flipped 21 times";
+break;
+fi
+((flip++))
+done
+
+echo "heads are $heads & tails are $tails";
+
+c=$flip;
+
+d=`echo $heads $c | awk '{print $1/$2}'`;
+e=`echo $tails $c | awk '{print $1/$2}'`;
+headspercentage=`echo $d 100 | awk '{print $1*$2}'`;
+tailspercentage=`echo $e 100 | awk '{print $1*$2}'`;
+
+echo " heads winning percentage is " $headspercentage;
+echo " tails winning percentage is " $tailspercentage;
+
